3885cd1bb3b715cb22c291ea46ee0ebb4855cbd1
HyungKyu Song <hk76.song@samsung.com>
2013-02-16 00:12:12 +0900

    Tizen 2.0 Release

---
+++ bt-share/include/bt-share-main.h
@@ -0,0 +1,77 @@
+/*
+ * bluetooth-share
+ *
+ * Copyright (c) 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __DEF_BLUETOOTH_SHARE_H_
+#define __DEF_BLUETOOTH_SHARE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <glib.h>
+#include <dbus/dbus-glib.h>
+#include <notification.h>
+
+#ifndef PACKAGE_NAME
+#define PACKAGE_NAME "bluetooth-share"
+#endif
+
+#define UI_PACKAGE "org.tizen.bluetooth-share-ui"
+#define UI_PKG_PATH "/opt/apps/org.tizen.bluetooth-share-ui/bin/bluetooth-share-ui"
+
+#define BT_VCONF_OPP_SERVER_INIT "memory/private/bluetooth-share/opp_server_init"
+
+typedef void (*bt_app_cb) (void *, void *, void *);
+
+typedef struct {
+	bt_app_cb popup_cb;
+	void *popup_cb_data;
+	gboolean syspopup_request;
+} bt_app_popups_t;
+
+typedef struct {
+	int tr_success;
+	int tr_fail;
+} bt_transfer_status_info_t;
+
+struct bt_appdata {
+	bt_app_popups_t popups;
+	bt_transfer_status_info_t send_data;
+	bt_transfer_status_info_t recv_data;
+	GSList *tr_send_list;
+	GSList *tr_next_data;
+	int current_tr_uid;
+	notification_h send_noti;
+	notification_h receive_noti;
+	notification_h opc_noti;
+	int opc_noti_id;
+	DBusGConnection *conn;
+	GObject *object;
+	gboolean obex_server_init;
+};
+
+
+void _bt_terminate_app(void);
+
+int _bt_init_obex_server(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* __DEF_BLUETOOTH_SHARE_H_ */
---
+++ bt-share/include/obex-event-handler.h
@@ -0,0 +1,63 @@
+/*
+ * bluetooth-share
+ *
+ * Copyright (c) 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __BT_EVENT_HANDLER_H__
+#define __BT_EVENT_HANDLER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "bluetooth-api.h"
+
+#define BT_FILE_PATH_LEN_MAX	(4096 + 10)
+#define BT_TEMP_FILE_PATH_LEN_MAX 262
+#define BT_TEXT_LEN_MAX 255
+
+typedef enum {
+	BT_FILE_IMAGE,	/**<IMAGE */
+	BT_FILE_VCARD,	/**<VCARD */
+	BT_FILE_VCAL,	/**<VCAL */
+	BT_FILE_VBOOKMARK,	/**<VBOOKMARK */
+	BT_FILE_VMEMO,
+	BT_FILE_DOC,	/**<DOC, */
+	BT_FILE_OTHER	/**<OTHER*/
+} bt_file_type_t;
+
+int _bt_get_transfer_id_by_noti_id(int noti_id);
+
+void _bt_clear_receive_noti_list(void);
+
+void _bt_share_event_handler(int event, bluetooth_event_param_t *param,
+			       void *user_data);
+
+void _bt_obex_server_event_handler(int event,
+				       bluetooth_event_param_t *param,
+				       void *user_data);
+
+void _bt_app_obex_download_dup_file_cb(void *data, void *obj,
+				       void *event_info);
+
+void _bt_get_default_storage(char *storage);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* __BT_EVENT_HANDLER_H__ */
