ab31110f7b6450c944001581d8525739a163e24c
DoHyun Pyun <dh79.pyun@samsung.com>
2016-04-27 17:25:56 +0900

    Remove dbus-glib-1 dependency
    Change-Id: I3b315707197deffc86cd82fd0c78e12694db1c4e
    Signed-off-by: DoHyun Pyun <dh79.pyun@samsung.com>

--- bt-share/src/bt-share-ipc.c
+++ bt-share/src/bt-share-ipc.c
@@ -17,8 +17,6 @@
  *
  */
 
-#include <dbus/dbus-glib-lowlevel.h>
-#include <dbus/dbus-glib.h>
 #include <dbus/dbus.h>
 #include <glib.h>
 #include <time.h>
@@ -421,20 +419,22 @@ static DBusHandlerResult __event_filter(DBusConnection *sys_conn,
 gboolean _bt_init_dbus_signal(void)
 {
 	DBG("+");
-	DBusGConnection *conn;
-	GError *err = NULL;
 	DBusError dbus_error;
 
-	conn = dbus_g_bus_get(DBUS_BUS_SYSTEM, &err);
-	if (!conn) {
+	dbus_error_init(&dbus_error);
+
+	dbus_connection = dbus_bus_get(DBUS_BUS_SYSTEM, &dbus_error);
+
+	if (!dbus_connection) {
 		ERR(" DBUS get failed");
-		g_error_free(err);
+		if (dbus_error_is_set(&dbus_error)) {
+			ERR("D-Bus Error: %s\n", dbus_error.message);
+			dbus_error_free(&dbus_error);
+		}
 		return FALSE;
 	}
-	dbus_connection = dbus_g_connection_get_connection(conn);
 
 	/* Add the filter for network client functions */
-	dbus_error_init(&dbus_error);
 	dbus_connection_add_filter(dbus_connection, __event_filter, NULL, NULL);
 	dbus_bus_add_match(dbus_connection,
 			"type=signal,interface=" BT_BLUEZ_INTERFACE
